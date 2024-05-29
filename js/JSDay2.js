47b01e7691e9fb47d93a3b364dc65b5b48069b0e
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2024-05-29 22:10:09 +0300

    fix bugs

--- js/JSDay2.js
+++ js/JSDay2.js
@@ -1,47 +1,44 @@
-const x = 3;
+function messageReturn(value1, value2) {
+  return `${value1} ${value2}`;
+}
 function message(message1, message2) {
+  const x = 3;
   console.log(x);
   if (x >= 3) {
     document.getElementById('pdi1').innerHTML = message1;
-    const y = 10;
     console.log('done local variable:', message2);
   }
-  const message_re = message_return('Alex', 'boom');
-  alert(message_re);
+  const messageRe = messageReturn('Alex', 'boom');
+  alert(messageRe);
+
+  const person = {
+    firsName: 'hi',
+    lastname: 'bye',
+    display1() {
+      return this.lastname;
+    },
+  };
+  person.firsName = 'John';
+  const stud = {};// const stud=new Object();
+  stud.grade = 23;
+  stud.sec = 'A';
+
+  let fullName = '';
+
+  Object.values(person).forEach((value) => {
+    fullName += `${value} `;
+  });
+  return fullName;
 }
-
-function message_return(value1, value2) {
-  return `${value1} ${value2}`;
-}
-
-const person = {
-  firsName: 'hi',
-  lastname: 'bye',
-  display1() {
-    return this.lastname;
-  },
-};
-person.firsName = 'John';
-const stud = {};// const stud=new Object();
-stud.grade = 23;
-stud.sec = 'A';
-
-const name1 = person.firsName;
-const lastname1 = person.lastname;
-const func = person.display1;
-
-let fullName = '';
-
-for (const data in person) {
-  fullName += `${person[data]} `;
-}
-const arr = Object.values(person);
-const json1 = JSON.stringify(person);
-// document.getElementById('id1').innerHTML=Date() + " ";
+message('cs', 'cci');
+// const arr = Object.values(person);
+// const json1 = JSON.stringify(person);
+// document.getElementById('id1').innerHTML = `${json1} `;
 const str = 'CS\'s';
 console.log(str.toLowerCase());
-const sec = new Array('a', 'b', 'v');
+const sec = ['a', 'b', 'v'];
 const sec1 = [];
 sec1[0] = 'k';
 document.write(sec.toString);
 sec1.sort();
+return 'done';
