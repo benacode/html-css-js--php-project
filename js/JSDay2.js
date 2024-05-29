9baecc4bc93f3e3d4d87c6749aee6feaec53a9f0
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2024-05-29 21:50:03 +0300

    fix bugs

--- js/JSDay2.js
+++ js/JSDay2.js
@@ -1,53 +1,47 @@
-let x=3
-function message(message1, message2)
-{
-    console.log(x);
-    if(x>=3)
-        {
-        document.getElementById('pdi1').innerHTML=message1;
-          var y=10; 
-          console.log("done local variable:",message2) 
-        }
-       let message_re= message_return('Alex',"boom")
-       alert(message_re);
+const x = 3;
+function message(message1, message2) {
+  console.log(x);
+  if (x >= 3) {
+    document.getElementById('pdi1').innerHTML = message1;
+    const y = 10;
+    console.log('done local variable:', message2);
+  }
+  const message_re = message_return('Alex', 'boom');
+  alert(message_re);
 }
 
-function message_return(value1, value2)
-{
-
-   return value1 +" " +value2;
+function message_return(value1, value2) {
+  return `${value1} ${value2}`;
 }
 
-const person={firsName:"hi",lastname:"bye",
-display1: function(){
+const person = {
+  firsName: 'hi',
+  lastname: 'bye',
+  display1() {
     return this.lastname;
-}
+  },
 };
-person.firsName="John";
-const stud={}//const stud=new Object();
-stud.grade=23;
-stud.sec="A";
+person.firsName = 'John';
+const stud = {};// const stud=new Object();
+stud.grade = 23;
+stud.sec = 'A';
 
-let name1=person['firsName'];
-let lastname1=person['lastname'];
-let func=person['display1'];
+const name1 = person.firsName;
+const lastname1 = person.lastname;
+const func = person.display1;
 
-let fullName='';
+let fullName = '';
 
-
-
-
-for(let data in person){
-    fullName +=person[data]+" ";
-
+for (const data in person) {
+  fullName += `${person[data]} `;
 }
-const arr=Object.values(person);
-let json1=JSON.stringify(person)
-//document.getElementById('id1').innerHTML=Date() + " ";
- let str='CS\'s'
- console.log(str.toLowerCase)
- let sec=new Array('a','b','v');
- let sec1=[];
- sec1[0]='k';
-document.write(sec.toString)
+const arr = Object.values(person);
+const json1 = JSON.stringify(person);
+// document.getElementById('id1').innerHTML=Date() + " ";
+const str = 'CS\'s';
+console.log(str.toLowerCase());
+const sec = new Array('a', 'b', 'v');
+const sec1 = [];
+sec1[0] = 'k';
+document.write(sec.toString);
 sec1.sort();
