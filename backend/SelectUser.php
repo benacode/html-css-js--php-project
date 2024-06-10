ba0a33dcf2ab06e4130ad7b5feb9afaa93b915a9
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2024-06-10 16:51:38 +0300

    add action button

--- backend/SelectUser.php
+++ backend/SelectUser.php
@@ -17,26 +17,34 @@ $sql = "SELECT id, username, firstname FROM users1";
 $result = $link ->query($sql);
 
 if ($result->num_rows > 0) {
-   
-
     echo "<table>
             <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>First Name</th>
+                <th>Actions</th>
             </tr>";
     while($row = $result->fetch_assoc()) {
         echo "<tr>
                 <td>" . $row["id"] . "</td>
                 <td>" . $row["username"] . "</td>
                 <td>" . $row["firstname"] . "</td>
+                <td>
+                    <form method='post' action='delete.php' style='display:inline;'>
+                        <input type='hidden' name='id' value='" . $row["id"] . "'>
+                        <input type='submit' value='Delete'>
+                    </form>
+                    <form method='post' action='update.php' style='display:inline;'>
+                        <input type='hidden' name='id' value='" . $row["id"] . "'>
+                        <input type='submit' value='Update'>
+                    </form>
+                </td>
               </tr>";
     }
     echo "</table>";
 } else {
     echo "0 results";
 }
-
 $link->close();
 ?>
 
