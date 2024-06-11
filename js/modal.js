9129067d26657e350924e4cc1c14f3ad0b3d5815
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2024-06-11 21:36:40 +0300

    add modal for popup message

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var message = document.getElementById('modalMessage').getAttribute('data-message');
+    var status = document.getElementById('modalMessage').getAttribute('data-status');
+
+    // Show the modal with the message
+    $('#modalMessage').text(message);
+    let url1;
+    if (status === "success") {
+        $('#messageModalLabel').text("Success");
+        url1='index.php'
+    } else {
+        $('#messageModalLabel').text("Error");
+        url1='signup.php'
+    }
+    $('#messageModal').modal('show');
+    
+    document.getElementById('closeModalButton').addEventListener('click', function() {
+        window.location.href = url1; 
+    
+    });
+});
